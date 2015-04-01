af4e22ce5722f0ea263d9c04e65fbed33224f2cf
ckeyer <wangcj1214@gmail.com>
2015-04-01 22:18:45 +0800

    add tag.go

---
+++ models/tag.go
@@ -0,0 +1,27 @@
+/*
+**/
+
+package models
+
+import (
+	"log"
+)
+
+type Tag struct {
+	Id       int
+	ParentId int
+	Name     string
+	Blogs    []int
+}
+
+func NewTag(name string) *Tag {
+	return &Tag{Name: name}
+}
+
+func GetTagByBlogId(blog_id int) *Tag {
+	return &Tag{}
+}
+
+func (this *Tag) name() {
+
+}
