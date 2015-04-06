975653c065c000000b070613404472f5ae0bd0c8
王传健 <ckeyer@users.noreply.github.com>
2015-04-06 12:24:44 +0800

    up

--- models/review.go
+++ models/review.go
@@ -10,7 +10,9 @@ Title string
 Content string
 Author string
 Email string
+ParentType int
+ParentId  int64
 }
 
 func NewReview(title,content,author,email string){
-}
+return &Review{Title:title,Content:content}
