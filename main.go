27e94721c13d1f8c8118a9e38aac91f0ef36e180
ckeyer <wangcj1214@gmail.com>
2015-06-09 11:11:23 +0800

    up conf of https

--- main.go
+++ main.go
@@ -18,13 +18,14 @@ func main() {
 }
 
 func https_init() {
-	// beego.EnableHttpTLS = true
+	beego.EnableHttpTLS = true
 	// beego.HttpsPort = 443
 	beego.SessionOn = true
 	beego.SessionGCMaxLifetime = 2
+	beego.SessionName = "CkeyerSession"
 	beego.BeegoServerName = "ckeyer Server 1.0"
-	beego.HttpCertFile = "/var/www/https/cert.pem"
-	beego.HttpKeyFile = "/var/www/https/key.pem"
+	// beego.HttpCertFile = "/var/www/https/cert.pem"
+	// beego.HttpKeyFile = "/var/www/https/key.pem"
 }
 func db_init() {
 	models.RegistDB()
