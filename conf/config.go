149a51130aec4b5af10c9b85dc609bae9e5dc2e2
ckeyer <me@ckeyer.com>
2015-08-18 00:47:34 +0800

    conf

--- conf/config.go
+++ conf/config.go
@@ -1,6 +1,12 @@
 package conf
 
 import (
+	"io/ioutil"
+	"os"
+
+	"encoding/json"
+
+	"github.com/astaxie/beego"
 	"github.com/ckeyer/go-lib"
 )
 
@@ -13,4 +19,35 @@ type Config struct {
 type App struct {
 	Post int
 	Name string
+}
+
+var config *Config
+
+// init 配置相关初始化
+func init() {
+	path := beego.AppConfig.String("config_path")
+	if path == "" {
+		return
+	}
+	config = new(Config)
+	config.Load(path)
+}
+
+// (c *Config)Load 加载配置
+func (c *Config) Load(path string) (err error) {
+	var conf Config
+
+	f, err := os.OpenFile(path, os.O_RDONLY, 0600)
+	if err != nil {
+		return
+	}
+
+	bs, err := ioutil.ReadAll(f)
+	if err != nil {
+		return
+	}
+
+	err = json.Marshal(bs, &conf)
+	c = &conf
+	return
 }
